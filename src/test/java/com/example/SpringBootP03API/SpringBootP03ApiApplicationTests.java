61175dee3f3ffad3e3a2914c059fee249f2597ba
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-25 15:09:12 +0530

    Initial commit

---
+++ src/test/java/com/example/SpringBootP03API/SpringBootP03ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootP03API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootP03ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

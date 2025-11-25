3fffab9f84bddb6c55b33a5b6fff329d20e6da46
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-25 15:14:03 +0530

    commit

---
+++ src/main/java/com/example/SpringBootP03API/controller/StudentController.java
@@ -0,0 +1,17 @@
+package com.example.SpringBootP03API.controller;
+
+import com.example.SpringBootP03API.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+    @Autowired
+    StudentService studentService;
+
+    @GetMapping("/welcome")
+    public String dataFetchFromDb(){
+        return studentService.getData();
+    }
+}
